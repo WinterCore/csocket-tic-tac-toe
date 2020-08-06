38b645599fc135ba982d1a76cacbd4b1dc286ab9
WinterCore <hogobbl@gmail.com>
2020-08-07 00:49:02 +0200

    Make the server script accept a port as an arg

--- src/server/main.c
+++ src/server/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "macros.h"
 #include "socket.h"
-
-#define PORT 8080
+#include "helpers.h"
 
 int main(int argc, char **argv) {
-    int server_socket = setup_server_socket(PORT);
-    printf("Server is up and running on port %d\n", PORT);
+    if (argc < 2 || !str_is_numeric(*(argv + 1))) {
+        fprintf(stderr, "Usage: server <port>");
+        return 1;
+    }
+    int port = strtol(*(argv + 1), NULL, 10);
+    int server_socket = setup_server_socket(port);
+    printf("Server is up and running on port %d\n", port);
     fflush(stdout);
     async_handle_connections(server_socket);
 
