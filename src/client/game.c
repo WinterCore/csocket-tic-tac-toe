4832fb3f65dc9bf84faa6a16918d4a2826502b90
WinterCore <hogobbl@gmail.com>
2020-08-09 10:28:48 +0200

    This was supposed to be included in the previous commit but I'm too lazy to reset and recommit

--- src/client/game.c
+++ src/client/game.c
@@ -2,12 +2,21 @@
 #include <curses.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #include "game.h"
 #include "macros.h"
 #include "../helpers.h"
 
 #define ALT_BACKSPACE 127
+#define OPTS_MENU_WIDTH 40
+#define OPTS_MENU_HEIGHT 4
+
+#define CREATE_GAME_OPT 0
+#define JOIN_GAME_OPT 1
+
+char *name = "Hasan", *shape = "x";
 
 void init_game(int sock) {
     initscr();
@@ -16,56 +25,125 @@ void init_game(int sock) {
     noecho();
     clear();
 
-    int centery = LINES / 2, centerx = COLS / 2;
+    int centerx = COLS / 2,
+        centery = LINES/ 2;
+
+    // set_initial_data();
+
+    print_str(centery - 3, "Choose an option wisely!");
+
+    char *opts[2] = {"Create a new game", "Join a game"};
+    int option = show_options_menu(centerx, centery + 1, opts, 2);
+
+    clear();
+
+
+    getch();
+    endwin();
+}
+
+int show_options_menu(int posx, int posy, char **opts, int optsc) {
+    int key, highlight = 0,
+        marginx = posx - OPTS_MENU_WIDTH / 2,
+        marginy = posy - OPTS_MENU_HEIGHT / 2;
+
+
+    WINDOW *menu = newwin(
+                             OPTS_MENU_HEIGHT,
+                             OPTS_MENU_WIDTH,
+                             marginy,
+                             marginx
+                         );
+
+    keypad(menu, true);
+
+    box(menu, 0, 0);
+    wrefresh(menu);
+
+    while (key != '\n') {
+        for (int i = 0; i < optsc; i += 1) {
+            if (i == abs(highlight % optsc))
+                wattron(menu, A_REVERSE);
+            mvwprintw(menu, i + 1, OPTS_MENU_WIDTH / 2 - strlen(opts[i]) / 2, opts[i]);
+            wattroff(menu, A_REVERSE);
+        }
+        key = wgetch(menu);
+
+        switch (key) {
+        case KEY_UP:
+            highlight -= 1;
+            break;
+        case KEY_DOWN:
+            highlight += 1;
+            break;
+        }
+    }
+    delwin(menu);
+
+    return abs(highlight % optsc);
+}
+
+void set_initial_data() {
+    int centery = LINES / 2;
 
     show_logo();
     refresh();
     sleep(1);
-    char message[50];
-    sprintf(message, "%s", "Welcome to Tic-Tac-Toe");
-    mvaddstr(centery + 1, centerx - strlen(message) / 2, message);
-    refresh();
+    print_str(centery + 1, "Welcome to Tic-Tac-Toe");
     sleep(1);
-    sprintf(message, "%s", "Brought to you by bender's ass");
-    mvaddstr(centery + 2, centerx - strlen(message) / 2, message);
-    refresh();
-
+    print_str(centery + 2, "Brought to you by bender's ass");
     sleep(1);
+    print_str(centery + 4, "Please Enter Your Name");
 
-    sprintf(message, "%s", "Please Enter Your Name: ");
-    mvaddstr(centery + 4, centerx - strlen(message) / 2, message);
+    name = read_str(centery + 5, 40);
+
+    clear_lines(centery + 4, centery + 6);
+
+    print_str(centery + 4, "Please Enter Your Shape");
+    shape = read_str(centery + 5, 3);
+}
+
+void print_str(int ypos, char *str) {
+    mvaddstr(ypos, COLS / 2 - strlen(str) / 2, str);
+    refresh();
+}
+
+void clear_lines(int start, int end) {
+    while (start <= end) {
+        move(start++, 0);
+        clrtoeol();
+    }
+    refresh();
+}
+
+char *read_str(int ypos, int max) {
+    char *name = malloc(max + 1);
+    int ch, chari = 0, centerx = COLS / 2;
+
+    move(ypos, centerx);
     refresh();
 
-    move(centery + 6, centerx);
-
-    char name[40]; // chars
-    int ch, chari = 0;
     while ((ch = getch()) != '\n') {
-        if (chari >= 40) {
-            continue;
-        }
+        if (chari >= max + 1) continue;
+
         if (ch == ALT_BACKSPACE) {
             if (chari > 0) {
                 chari -= 1;
-                move(centery + 6, 0);
-                refresh();
+                move(ypos, 0);
                 clrtoeol();
-                mvaddnstr(centery + 6, centerx - chari / 2, name, chari);
+                mvaddnstr(ypos, centerx - chari / 2, name, chari);
                 refresh();
             }
         } else {
             name[chari] = ch;
             chari += 1;
 
-            mvaddnstr(centery + 6, centerx - chari / 2, name, chari);
+            mvaddnstr(ypos, centerx - chari / 2, name, chari);
         }
     }
     name[chari] = '\0';
 
-
-    refresh();
-    getch();
-    endwin();
+    return name;
 }
 
 
--- src/client/game.h
+++ src/client/game.h
@@ -2,6 +2,12 @@
 #define GAME_H
 
 void init_game(int sock);
+void set_initial_data();
+
 void show_logo();
+void print_str(int ypos, char *str);
+char *read_str(int ypos, int max);
+void clear_lines(int start, int end);
+int show_options_menu(int pos, int posy, char **opts, int optsc);
 
 #endif
